fb6d770ba66bcffcf82fee995b4b7ea57ee28a7d
Fadel Akram <af8356207@gmail.com>
2025-11-10 18:44:23 +0100

    used binding, added next button and question data class

--- geo_quiz/app/src/main/java/com/example/geo_quiz/MainActivity.kt
+++ geo_quiz/app/src/main/java/com/example/geo_quiz/MainActivity.kt
@@ -18,21 +18,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var trueButton : Button
-    private lateinit var falseButton : Button
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton.setOnClickListener { view : View ->
-            Snackbar.make(trueButton, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+
+        binding.trueButton.setOnClickListener { view : View ->
+            Snackbar.make(binding.trueButton, R.string.correct_toast, Toast.LENGTH_SHORT).show()
         }
 
-        falseButton.setOnClickListener { view : View ->
-            Snackbar.make(falseButton, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        binding.falseButton.setOnClickListener { view : View ->
+            Snackbar.make(binding.falseButton, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionText.setText(questionTextResId)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
